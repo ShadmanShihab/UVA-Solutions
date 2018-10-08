a69e4ca24ae2795c905cb7e82eb1af5a1fad48fc
shadman_shihab <33175089+ShadmanShihab@users.noreply.github.com>
2018-10-08 20:01:09 +0600

    Add files via upload

---
+++ uva 11498.cpp
@@ -0,0 +1,38 @@
+//uva 11498
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int k, n, m, x, y;
+
+    while(1)
+    {
+        scanf("%d", &k);
+
+        if(k == 0)
+            break;
+        scanf("%d %d" , &n, &m);
+
+        for(int i=0; i<k; i++)
+        {
+             scanf("%d %d", &x, &y);
+
+             if(x==n || y==m)
+                printf("divisa\n");
+
+             else if(x>n && y>m)
+                printf("NE\n");
+
+             else if(x>n && y<m)
+                printf("SE\n");
+
+            else if(x<n && y>m)
+                printf("NO\n");
+
+            else if(x<n && y<m)
+                printf("SO\n");
+        }
+    }
+    return 0;
+}
